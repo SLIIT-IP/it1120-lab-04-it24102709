ba856029f7cd2bbfd6f001652beeb12966281fab
it24102709 <it24102709@my.sliit.lk>
2024-08-16 11:49:34 +0530

    it24102709Lab4Q1

---
+++ Q1/IT24102709Lab4Q1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class IT24102709Lab4Q1 {
+	public static void main(String[] args) {
+	     Scanner input = new Scanner(System.in);
+		
+	
+   System.out.println("Enter a number : ");
+  double number = input.nextDouble();
+	 if(number<0) {
+ 		System.out.println("The number is - Negative"); }
+    
+    else if (number>0) {
+		System.out.println("The number is - Positive");  }
+else { 
+
+		System.out.println("The number is Zero "); }
+	}
+}
+	
+	     
